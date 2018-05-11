031c8eee9820443e04e9c56606b9b474bd855c24
EspiDev <espidev@gmail.com>
2018-05-10 21:39:17 -0400

    init

---
+++ src/main.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	fs := http.FileServer(http.Dir("static/"))
+	r.Handle("/site", http.StripPrefix("/site", fs))
+
+	http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
