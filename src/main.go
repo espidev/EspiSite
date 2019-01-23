03a16c9947d4b03e8e049cd625388c4e0bc961ac
EspiDev <espidev@gmail.com>
2019-01-22 21:40:24 -0500

    Switching out go base to 1.11

--- src/main.go
+++ src/main.go
@@ -1,64 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
-	"net/http"
-	"time"
-	"html/template"
+	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
+	"strconv"
 )
 
-type Variables struct {
-	Time string
-}
+var (
+	router *gin.Engine
+	config Config
+)
 
 func main() {
-	log.Printf("Starting EspiSite...")
-	log.Printf("Assembling files...")
+	log.Printf("Starting EspiSite...\n")
+	setupConfig()
 
-	assemble() //gather files
+	router = gin.Default()
+	setupRoutes()
 
-	log.Printf("Initializing routers...")
-
-	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler)
-	r.HandleFunc("/home", homeHandler)
-	fs := http.FileServer(http.Dir("static/"))
-	r.Handle("/site", http.StripPrefix("/site", fs))
-
-	log.Printf("Webserver starting on port 8080.")
-
-	http.ListenAndServe(":8080", r)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	vars := Variables{}
-	t, err := template.ParseFiles("./html/index.html")
+	err := router.Run(":" + strconv.Itoa(config.Port))
 	if err != nil {
-		log.Print("template parsing error: ", err)
-	}
-	err = t.Execute(w, vars)
-	if err != nil {
-		log.Print("template parsing error: ", err)
+		log.Fatal(err)
 	}
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	now := time.Now()
-	vars := Variables{
-		Time: now.Format("15:04:05"),
-	}
-	t, err := template.ParseFiles("./html/home.html")
-	if err != nil {
-		log.Print("template parsing error: ", err)
-	}
-	err = t.Execute(w, vars)
-	if err != nil {
-		log.Print("template parsing error: ", err)
-	}
+func setupRoutes() {
+	router.GET("/", func (c *gin.Context) {
+		c.String(http.StatusOK, "HI")
+	})
 }
