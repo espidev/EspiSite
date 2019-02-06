d370a954b8b42bd641a9cacf891fbe5bc6ebebc7
EspiDev <espidev@gmail.com>
2019-02-06 13:49:11 -0500

    Fix routes

--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"time"
 )
 
 var (
@@ -22,14 +27,35 @@ func main() {
 	router = gin.Default()
 	setupRoutes()
 
-	err := router.Run(":" + strconv.Itoa(config.Port))
-	if err != nil {
-		log.Fatal(err)
+	srv := &http.Server {
+		Addr: ":" + strconv.Itoa(config.Port),
+		Handler: router,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	quit := make (chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<- quit
+	log.Println("Shutting down EspiSite...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown: ", err)
+	}
+	log.Println("EspiSite has stopped.")
 }
 
 func setupRoutes() {
-	router.LoadHTMLFiles("/src/html/*")
-	router.Static("/", RootRepoFolder + "/src/html/")
+	router.LoadHTMLFiles(RootRepoFolder + "/src/html/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+	router.GET("/test", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home.html", gin.H{})
+	})
 }
-g
