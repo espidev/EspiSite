e9c1e865ac522e94c178a03048c8df2ab8ea1f5a
EspiDev <espidev@gmail.com>
2019-03-08 14:00:07 -0500

    Work on post page

--- src/main/main.go
+++ src/main/main.go
@@ -1,5 +1,23 @@
 package main
 
+/*
+    EspiSite - a quick and dirty CMS
+    Copyright (C) 2019 EspiDev
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import (
 	"context"
 	"github.com/gin-gonic/gin"
@@ -23,6 +41,10 @@ const (
 
 func main() {
 	log.Printf("Starting EspiSite...\n")
+
+	// License disclaimer
+	log.Println(`This program comes with ABSOLUTELY NO WARRANTY;\n
+    This is free software, and you are welcome to redistribute it under certain conditions.`)
 
 	// Load config and data
 	setupConfig()
